1dc1ad90a360cbc8f75ef299ae33cff2f907ae8b
urbanjost <urbanjost@comcast.net>
2023-04-12 03:14:25 +0000

    Deploying to gh-pages from  @ 6a97f27fa6cbc7510c0af50abab3271e61a1ddb1 🚀

--- docs/fpm-ford/src/demo_system_getgrgid.f90
+++
@@ -1,8 +0,0 @@
-          program demo_system_getgrgid
-          use M_system, only : system_getgrgid
-          use M_system, only : system_getgid
-          implicit none
-          character(len=:),allocatable :: name
-          name=system_getgrgid( system_getgid() )
-          write(*,'("group[",a,"] for ",i0)')name,system_getgid()
-          end program demo_system_getgrgid

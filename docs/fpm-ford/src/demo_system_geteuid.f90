c7513e5d333af5e2a4477b4b7b7e342ec2481bc2
urbanjost <urbanjost@comcast.net>
2025-07-29 02:35:13 +0000

    Deploying to gh-pages from  @ ee6f20e1028ce889ca6539789ad64a8e54261dd2 🚀

--- docs/fpm-ford/src/demo_system_geteuid.f90
+++
@@ -1,5 +0,0 @@
-     program demo_system_geteuid
-     use M_system, only : system_geteuid
-     implicit none
-        write(*,*)'EFFECTIVE UID=',system_geteuid()
-     end program demo_system_geteuid
